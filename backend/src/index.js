a568b631ce1b2183b477434214f3aa3731c16b0e
Tejas Gund <tejasgund111@gmail.com>
2024-11-27 10:56:56 +0530

    Issue fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,7 +21,7 @@ dotenv.config();
 const PORT = process.env.PORT;
 
 app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoute);
+app.use("/api/messages", messageRoute);
 
 app.listen(PORT, () => {
     console.log("Server is running on port", PORT);
