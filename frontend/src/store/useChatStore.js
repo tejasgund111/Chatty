7fe1b4dc0f6b520136efb120472bfaa083f76406
Tejas Gund <tejasgund111@gmail.com>
2024-11-27 12:15:23 +0530

    Homepage-Chat Container created

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -33,15 +33,15 @@ export const useChatStore = create((set, get) => ({
       set({ isMessagesLoading: false });
     }
   },
-  // sendMessage: async (messageData) => {
-  //   const { selectedUser, messages } = get();
-  //   try {
-  //     const res = await axiosInstance.post(`/messages/send/${selectedUser._id}`, messageData);
-  //     set({ messages: [...messages, res.data] });
-  //   } catch (error) {
-  //     toast.error(error.response.data.message);
-  //   }
-  // },
+  sendMessage: async (messageData) => {
+    const { selectedUser, messages } = get();
+    try {
+      const res = await axiosInstance.post(`/messages/send/${selectedUser._id}`, messageData);
+      set({ messages: [...messages, res.data] });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  },
 
   // subscribeToMessages: () => {
   //   const { selectedUser } = get();
