dce7413c4db33229e8557ef81bcad2a084ba02de
Tejas Gund <tejasgund111@gmail.com>
2024-11-27 13:29:12 +0530

    Implemented socket.io

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -2,6 +2,8 @@ import User from "../models/user.model.js"
 import Message from "../models/message.model.js"
 import cloudinary from "../lib/cloudinary.js";
 
+import { getReceiverSocketId, io } from "../lib/socket.js";
+
 export const getUsersForSidebar = async (req, res) => {
     try {
         const loggedInUserId = req.user._id;
@@ -38,33 +40,35 @@ export const getMessages = async (req, res) => {
 }
 
 export const sendMessage = async (req, res) => {
-    try{
-        const {text, image} = req.body;
-        const {id : receiverId} = req.params;
-        const senderId = req.user._id;
-
-        let imageUrl;
-        if(image){
-            // upload base64 image to cloudinary
-            const uploadResponse = await cloudinary.uploader.upload(image);
-            imageUrl = uploadResponse.secure_url;
-        }
-
-        const newMessage = new Message ({
-            senderId,
-            receiverId,
-            text, 
-            image : imageUrl,
-        });
-
-        await newMessage.save();
-
-        // todo : realtime functionlity goes here => socket.io
-        
-        res.status(201).json(newMessage);
+    try {
+      const { text, image } = req.body;
+      const { id: receiverId } = req.params;
+      const senderId = req.user._id;
+  
+      let imageUrl;
+      if (image) {
+        // Upload base64 image to cloudinary
+        const uploadResponse = await cloudinary.uploader.upload(image);
+        imageUrl = uploadResponse.secure_url;
+      }
+  
+      const newMessage = new Message({
+        senderId,
+        receiverId,
+        text,
+        image: imageUrl,
+      });
+  
+      await newMessage.save();
+  
+      const receiverSocketId = getReceiverSocketId(receiverId);
+      if (receiverSocketId) {
+        io.to(receiverSocketId).emit("newMessage", newMessage);
+      }
+  
+      res.status(201).json(newMessage);
+    } catch (error) {
+      console.log("Error in sendMessage controller: ", error.message);
+      res.status(500).json({ error: "Internal server error" });
     }
-    catch(error){
-        console.log("Error in sendMessages: ", error.message);
-        res.status(500).json({ error: "Internal server error" });
-    }
-}
+  };
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,29 +1,42 @@
-// const express = require("express")
 import express from "express";
 import dotenv from "dotenv";
-import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
-import authRoutes from "./routes/auth.route.js";
-import messageRoute from "./routes/message.route.js";
+import path from "path";
 
-const app = express();
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
+app.use(
+  cors({
     origin: "http://localhost:5173",
     credentials: true,
-}));
-
-dotenv.config();
-const PORT = process.env.PORT;
+  })
+);
 
 app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoute);
+app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
-    console.log("Server is running on port", PORT);
-    connectDB();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
 });
